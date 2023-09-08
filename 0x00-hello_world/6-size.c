3de268ab1d7d6c9e71a92dce08b36744088d79aa
Nisrine2001 <nisrine.touijer@gmail.com>
2023-09-07 17:46:49 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+char a;
+int b;
+long int c;
+long long int d;
+float e;
 
-printf("size of a char: %lu byte(s)\n", sizeof(char));
-printf("size of int: %lu byte(s)\n", sizeof(int));
-printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("size of a float: %lu byte(s)\n", sizeof(float));
+printf("size of a char: %lu byte(s)\n", sizeof(a));
+printf("size of int: %lu byte(s)\n", sizeof(b));
+printf("size of a long int: %lu byte(s)\n", sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("size of a float: %lu byte(s)\n", sizeof(e));
 return (0);
 }
