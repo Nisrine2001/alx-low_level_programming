eb558291cfd399ea236b4beb704eb02d5067e0a5
Nisrine2001 <nisrine.touijer@gmail.com>
2023-09-13 07:06:25 -0700

    Task 1

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -5,11 +5,10 @@
  *
  * Return: Always 0.
  */
-/* Declaration of the print_alphabet function */
 void print_alphabet();
-
-int main() {
-    /* Call the print_alphabet function */
-    print_alphabet();
-    return 0;
+int main() 
+{
+  print_alphabet();
+  return 0;
 }
+
