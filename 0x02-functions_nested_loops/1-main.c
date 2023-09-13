d34c3eaf5948c125f690f1b798cb44c555677aaa
Nisrine2001 <nisrine.touijer@gmail.com>
2023-09-12 17:27:30 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+/* Declaration of the print_alphabet function */
+void print_alphabet();
+
+int main()
+{
+    /* Call the print_alphabet function */
+    print_alphabet();
+    return 0;
+}
